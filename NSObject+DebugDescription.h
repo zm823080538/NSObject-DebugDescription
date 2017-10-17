375b9f6fcdf21230a8b8cc75d933d375259a53e4
sunny <zm823080538@MacBook-Pro-3.local>
2017-10-18 00:17:20 +0800

    no message

--- NSObject+DebugDescription.h
+++ NSObject+DebugDescription.h
@@ -5,14 +5,6 @@
 //  Created by sunny on 2017/7/23.
 //  Copyright © 2017年 sunny. All rights reserved.
 // test
-<<<<<<< HEAD
-//master-zm
-//5757657
-=======
-//zm
-//master-zm
-//1231231
->>>>>>> zm
 
 #import <Foundation/Foundation.h>
 ///hello world
